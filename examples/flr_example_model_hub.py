d43a4c0533d00a906f9151b78f7e4136c37a85f7
sebastianpinedaar <relea@informatik.uni-freiburg.de>
2025-07-09 21:27:10 +0200

    delete irrelevant examples

--- examples/flr_example_model_hub.py
+++
@@ -1,8 +0,0 @@
-from flr.hub.model_hub import ModelHub
-
-if __name__ == "__main__":
-    model_hub = ModelHub("llm_instruct")
-    print("Available models:")
-    for model in model_hub.get_models():
-        print(model)
-    print("\nModel attributes:")
